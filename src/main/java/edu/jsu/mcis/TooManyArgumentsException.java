6c80cb420f6399340518b2adc57dc609015d9e9a
Trey50Daniel <tagg8945@gmail.com>
2016-02-09 20:15:29 -0600

    Added Exception class

---
+++ src/main/java/edu/jsu/mcis/TooManyArgumentsException.java
@@ -0,0 +1,10 @@
+package edu.jsu.mcis.*;
+
+public class TooManyArgumentsException extends RuntimeException {
+	private String tooManyArgumentsMessage;
+	
+	
+	public TooManyArgumentsException(String extraArguments) {
+		
+	}
+}
