4befe115a302c4539ad12fcb7e1c31ea60f9931a
Trey50Daniel <tagg8945@gmail.com>
2016-02-10 21:40:04 -0600

    Updated batch files

--- rundemoprogram.bat
+++ rundemoprogram.bat
@@ -1,5 +1,4 @@
 cd "demo program"
 javac -cp .;..\build\libs\argsparser-1.0.jar VolumeCalculator.java
 java -cp .;..\build\libs\argsparser-1.0.jar VolumeCalculator 7 5 2
-timeout /t 10
-cd ..
+pause
--- rundemoprogramHelp.bat
+++ rundemoprogramHelp.bat
@@ -1,5 +1,4 @@
 cd "demo program"
 javac -cp .;..\build\libs\argsparser-1.0.jar VolumeCalculator.java
 java -cp .;..\build\libs\argsparser-1.0.jar VolumeCalculator -h
-timeout /t 30
-cd ..
+pause
