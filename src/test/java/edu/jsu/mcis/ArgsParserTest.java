402d66a6c24fc4015fac5175b7c4810a0919ac65
Jake Hambey <jhambey@stu.jsu.edu>
2016-02-15 11:59:06 -0600

    Consolidated all tests into one file

--- src/test/java/edu/jsu/mcis/ArgsParserTest.java
+++ src/test/java/edu/jsu/mcis/ArgsParserTest.java
@@ -39,4 +39,43 @@ public class ArgsParserTest {
 		p.addArg("test2");
 		assertEquals(2,p.getNumArguments());
 	}
+	
+	@Test
+	public void testHelpMessageFormattedCorrectly(){
+		String[] testCommandLineArgs = {"-h"};
+		String messageTest = "";
+		String message = "usage: java VolumeCalculator length width height\n"+
+					"Calcuate the volume of a box.\n"+
+					"positional arguments:\n"+
+					"length the length of the box (float)\n"+
+					"width the width of the box(float)\n"+
+					"height the height of the box(float)";
+		try{
+		p.parse(testCommandLineArgs);
+		} catch(HelpMessageException h){
+			messageTest = h.getMessage();
+			}
+		
+		assertEquals(message,messageTest);
+	}
+	
+	@Test
+	public void testExtraArgumentsAssignedCorrectly() {
+		ArgsParser p = new ArgsParser();
+		String [] testArgs = {"4", "6", "14", "72", "43"};
+		String extraArg = "initialvalue";
+		String extraArgMessage = "initialvalue";
+		
+		p.addArg("one");
+		p.addArg("two");
+		p.addArg("three");
+		try{
+		p.parse(testArgs);
+		}catch(TooManyArgumentsException t){
+			extraArg = t.getExtraArgs();
+			extraArgMessage = t.getMessage();
+		}
+		assertEquals("72 43",extraArg);
+		assertEquals("usage: java VolumeCalculator length width height\nVolumeCalculator.java: error: unrecognized arguments: 72 43", extraArgMessage);
+	}
 }
