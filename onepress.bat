76a6807a05dbc684a007b91c215a7b68d10ff35f
tristo7 <tristo7000@gmail.com>
2016-02-16 17:21:49 -0600

    made edits to open all 3 reports after both tests have run.

--- onepress.bat
+++ onepress.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 title Running: Gradle
 call gradle clean build jacocoTestReport
-start "" "file://%CD%\build\reports\tests\index.html"
-start "" "file://%CD%\build\reports\jacoco\test\html\index.html"
 title Running: Acceptance
 call runacceptance.bat
+start "" "file://%CD%\build\reports\tests\index.html"
+start "" "file://%CD%\build\reports\jacoco\test\html\index.html"
 start "" "file://%CD%\acceptance\report.html"
 pause
