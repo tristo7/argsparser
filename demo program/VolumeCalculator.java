c188f4ee29df7b9993d37b45160088b2d5368501
Trey50Daniel <tagg8945@gmail.com>
2016-02-19 12:19:59 -0600

    Added optional args for demo

--- demo program/VolumeCalculator.java
+++ demo program/VolumeCalculator.java
@@ -6,12 +6,15 @@ public class VolumeCalculator {
 		p.addArg("length", Arg.DataType.FLOAT);
 		p.addArg("width", Arg.DataType.FLOAT);
 		p.addArg("height", Arg.DataType.FLOAT);
+		p.addOptionalArg("type", Arg.DataType.STRING, "cube");
+		p.addOptionalArg("digits", Arg.DataType.INTEGER, 2);
 		
 		p.parse(args);
 		
 		float l = p.getArgValue("length"));
 		float h = p.getArgValue("height"));
 		float w = p.getArgValue("width"));
+		
 		
 		System.out.println("Volume is "+Math.round(l*w*h)+".");
 	}
