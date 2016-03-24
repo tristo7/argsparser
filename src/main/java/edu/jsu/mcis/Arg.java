dd9f45d5c289748dcf10f70b4d5bab93e3dd8ddb
tristo7 <tristo7000@gmail.com>
2016-03-24 16:27:55 -0500

    Javadocs
    *added support for command "javaDocs" to gradle. Generates javadocs for all src files.
    *added javadoc comments throughout the Arg class

--- src/main/java/edu/jsu/mcis/Arg.java
+++ src/main/java/edu/jsu/mcis/Arg.java
@@ -1,38 +1,76 @@
 package edu.jsu.mcis;
 
+/** Argument class. Holds information about either positional or named arguments.	
+*	
+*	@author Tristin Terry
+* 	@author Daniel Hilburn
+* 	@author Thomas Eyler
+* 	@author Jake Hamby
+* 	@author Amari Richardson
+*/	
 public class Arg {
+	/** 
+	*	Data types that are supported.
+	*/
     public enum DataType {INTEGER, FLOAT, BOOLEAN, STRING};
-    private String argumentName;
+    
+	private String argumentName;
 	private char argumentShortName;
     private Object val;
     private DataType dType = DataType.STRING;
 	private String argumentDescription = "";
 	private boolean isOptionalArgument = false;
-
+	
+	/** Constructor that sets the name of the argument.
+	*	@param name The name of the argument. 
+	*/	
     public Arg(String name) {
         argumentName = name;
     }
-
+	
+	/** Constructor that sets the name and data type of the argument.
+	*	@param name The name of the argument. 
+	* 	@param type The data type of the argument.
+	*/	
     public Arg(String name, DataType type) {
 		this(name);
 		dType = type;
     }
 	
+	/** Constructor that sets the name, data type, and description of the argument.
+	*	@param name The name of the argument. 
+	* 	@param type The data type of the argument.
+	* 	@param desc Description of the argument.
+			For example, an argument named "length" may have a description "The length of the box."
+	*/	
 	public Arg(String name, DataType type, String desc) {
         this(name, type);
 		argumentDescription = desc;
     }
 	
+	/** Constructor that sets the name, data type, description, and default value of the argument. <STRONG>Using this constructor indicates a named argument.</STRONG>
+	*	@param name The name of the argument. 
+	* 	@param type The data type of the argument.
+	* 	@param desc Description of the argument.
+			For example, an argument named "length" may have a description "The length of the box."
+	*	@param defaultValue The argument's default value entered as a string.
+	*/	
 	public Arg(String name, DataType type, String desc, String defaultValue){
 		this(name, type, desc);
 		isOptionalArgument = true;
 		setArgValue(defaultValue);
 	}
 	
+	/** Sets the description of the argument.
+	*	@param s Description of the argument.
+	*/	
 	public void setArgDescription(String s){
 		argumentDescription = s;
 	}
 	
+	/** Sets the short form name of a <STRONG>named argument</STRONG>.
+	*	@param c Character used to identify the named argument.
+	*/	
 	public void setArgShortName(char c){
 		if(isOptionalArgument)
 			argumentShortName = c;
@@ -40,14 +78,26 @@ public class Arg {
 			throw new InvalidArgumentException(argumentName + " is a positional argument.", this);
 	}
 	
+	/**
+	*	Gives the argument's descipriton.
+	*	@return String value of the argument's descipriton.
+	*/	
 	public String getArgDescription(){
 		return argumentDescription;
 	}
 	
+	/**
+	*	Gives the argument's name.
+	*	@return String value of the argument's name.
+	*/	
 	public String getArgName(){
 		return argumentName;
 	}
 	
+	/**
+	*	Gives the named argument's short form name.
+	*	@return character value of the named argument's short form name.
+	*/	
 	public char getArgShortName(){
 		if(isOptionalArgument)
 			if(argumentShortName != '\u0000')
@@ -57,11 +107,15 @@ public class Arg {
 		else
 			throw new InvalidArgumentException(argumentName + " is not a named argument.", this);
 	}
-
+	
+	/**
+	*	Returns the argument's data type as a String.
+	* 	@return String value of the argument's data type.
+	*/	
 	public String getDataType(){
 		return dType.toString().toLowerCase();
 	}
-	
+
     protected void setArgValue(String value) {
 		switch(dType){
 			case INTEGER:
@@ -84,11 +138,20 @@ public class Arg {
 				val = value;
         }
     }
-
+	
+	/** Returns the value of the argument as a generic type. Result must be cast to the proper data type.
+	*		Example cases: int i = p.getArgValue() or (int) p.getArgValue();
+	*	@param <T> Generic type for the argument. 
+	*	@return The argument's value as a generic T type. This should then be cast to the proper data type.
+	*/
     public <T> T getArgValue() {
         return (T) val;
     }
 	
+	/** 
+	*	Returns all data stored about the argument in an XML format.
+	*	@return XML data formatted as a string.
+	*/
 	public String toXML(){
 		String statement = "";
 		if(isOptionalArgument){
