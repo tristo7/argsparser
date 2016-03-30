48b0bbe368550b0916e9a8158e907ae97ad4570c
Trey50Daniel <tagg8945@gmail.com>
2016-03-29 19:51:32 -0500

    Added javadoc info for ArgsParser

--- src/main/java/edu/jsu/mcis/ArgsParser.java
+++ src/main/java/edu/jsu/mcis/ArgsParser.java
@@ -8,6 +8,14 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 
+/** Parser class for the Arguments
+*	
+*	@author Tristin Terry
+* 	@author Daniel Hilburn
+* 	@author Thomas Eyler
+* 	@author Jake Hamby
+* 	@author Amari Richardson
+*/
 public class ArgsParser {
 
 	private List<String> argNames;
@@ -17,6 +25,9 @@ public class ArgsParser {
 	private String programDescription = "";
 	private Map<Character, String> shortNameMap;
 	
+	/** Constructor that creates a new instance of ArgsParser.
+	*	Initializes new HashMaps and ArrayLists for storing Args.
+	*/
 	public ArgsParser() {
 		argNames = new ArrayList<String>();
 		optionalArgNames = new ArrayList<String>();
@@ -24,54 +35,109 @@ public class ArgsParser {
 		shortNameMap = new HashMap<Character, String>();
 	}
 	
+	/**
+	*	Gives the List of names of positional Args that ArgsParser has stored.
+	*	@return the List of names of positional Args currently being stored by ArgsParser.
+	*/
 	public List<String> getPositionalArgumentNames(){
 		return argNames;
 	}
+	
+	/**
+	*	Gives the List of names of named Args that ArgsParser has stored.
+	*	@return the List of names of named Args currently being stored by ArgsParser.
+	*/
 	public List<String> getOptionalArgumentNames(){
 		return optionalArgNames;
 	}
 	
+	/**
+	*	Gives the number of Args that ArgsParser has stored.
+	*	@return the number of Args currently being stored by ArgsParser.
+	*/
 	public int getNumArguments() {
 		return argNames.size()+optionalArgNames.size();
 	}
 	
+	/**
+	*	Sets the program name based upon a string from the user.
+	*	@param the name for the program
+	*/	
 	public void setProgramName(String name) {
 		programName = name;
 	}
 	
+	/**
+	*	Gives the program name specified by the user.
+	*	@return string containing the name of the program using ArgsParser
+	*/
 	public String getProgramName() {
 		return programName;
 	}
 	
+	/**
+	*	Sets the program description based upon a string from the user.
+	*	@param the string for the description of the program using ArgsParser.
+	*/	
 	public void setProgramDescription(String s) {
 		programDescription = s;
 	}
 	
+	
+	/**
+	*	Gives the program description specified by the user.
+	*	@return string containing the description of the program using ArgsParser
+	*/	
 	public String getProgramDescription() {
 		return programDescription;
 	}
 	
+	/**
+	*	Method to add an argument with the name of the argument.
+	*	@param the string for the name of the argument to be added.
+	*/
 	public void addArg(String name) {
 		argNames.add(name);
 		argMap.put(name, new Arg(name));
 	}
 	
+	/**
+	*	Method to add an argument with the name of the argument and description.
+	*	@param the string for the name of the argument to be added.
+	*	@param the description of the argument to be added.
+	*/
 	public void addArg(String name, String description) {
 		argNames.add(name);
 		argMap.put(name, new Arg(name, Arg.DataType.STRING, description));
 	}
 	
+	/**
+	*	Method to add an argument with the name of the argument and description.
+	*	@param the string for the name of the argument to be added.
+	*	@param the datatype of the argument to be added.
+	*/
 	public void addArg(String name, Arg.DataType myType) {
 		argNames.add(name);
 		argMap.put(name, new Arg(name, myType));
 	}
 	
+	/**
+	*	Method to add an argument with the name of the argument and description.
+	*	@param the string for the name of the argument to be added.
+	*	@param the datatype of the argument to be added.
+	*	@param the description of the argument to be added.
+	*/
 	public void addArg(String name, Arg.DataType myType, String description) {
 		argNames.add(name);
 		argMap.put(name, new Arg(name, myType, description));
 	}
 	
-	
+	/**
+	*	Adds an optional argument to the HashMap without the shortname
+	* 	@param String name of the optional argument.
+	*	@param DataType of the argument being added.
+	*	@param the default value for the argument being created.
+	*/
 	public void addOptionalArg(String name, Arg.DataType type, String defaultValue){
 		switch(type){
 			case BOOLEAN:
@@ -86,12 +152,23 @@ public class ArgsParser {
 		}
 	}
 	
+	/**
+	*	Adds an optional argument to the HashMap and name, shortname to the list of names, shortnames
+	* 	@param String name of the optional argument.
+	*	@param DataType of the argument being added.
+	*	@param the default value for the argument being created.
+	*	@param the character for the short name of the argument
+	*/
 	public void addOptionalArg(String name, Arg.DataType type, String defaultValue, char shortName){
 		shortNameMap.put(shortName, name);
 		addOptionalArg(name, type, defaultValue);
 		getArg(name).setArgShortName(shortName);
 	}
 
+	/**
+	*	Parses the data passed from the command line and stores arguments.
+	* 	@param String array of the command line arguments
+	*/
 	public void parse(String[] cla) {
 		String currentArg = "";
 		int currentPosArg = 0;
@@ -155,6 +232,10 @@ public class ArgsParser {
 		}
 	}
 	
+	/**
+	*	Gives an Arg based upon the given String name.
+	*	@return Arg object from the argMap given the name of the Arg.
+	*/	
 	public Arg getArg(String name) {
 		if(argMap.containsKey(name))
 			return argMap.get(name);
@@ -162,6 +243,10 @@ public class ArgsParser {
 			throw new RuntimeException("Argument " + name + " does not exist.");
 	}
 	
+	/**
+	*	Gives the value of an argument cast as type T.
+	*	@return type T representation of the argument's associated value given the name.
+	*/	
 	public <T> T getArgValue(String name) {
 		return (T) getArg(name).getArgValue();
 	}
